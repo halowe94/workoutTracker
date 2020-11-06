0f384f1bbcc5f6085099ec17f92ae9eb1bd15bbc
H Lowe <haleylowe0@gmail.com>
2020-11-06 00:36:32 -0800

    created htmlROutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,11 +2,23 @@ const Workout = require("../models/workout");
 
 module.exports = function(app) {
     //get route
+    app.get("/api/workouts", (req, res) => {
+
+    })
 
     //post route
+    app.post("/api/workouts", (req, res) => {
+
+    })
 
     //put route
+    app.put("/api/workouts", (req, res) => {
+
+    })
 
     //get route
+    app. get("api/workouts", (req, res) => {
+        
+    })
 
 };
