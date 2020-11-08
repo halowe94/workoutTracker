65eb0b55d6046f15c3283466051a3979bbc7858d
H Lowe <haleylowe0@gmail.com>
2020-11-07 20:26:30 -0800

    added api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,45 @@
+const { json } = require("express");
 const Workout = require("../models/workout");
 
 module.exports = function(app) {
     //get route
     app.get("/api/workouts", (req, res) => {
-
+        Workout.find({}).then(workoutDB => {
+            res.json(workoutDB);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
     })
 
     //post route
     app.post("/api/workouts", (req, res) => {
-
+        Workout.create({}).then(workoutDB => {
+            res.json(workoutDB);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        }); 
     })
 
     //put route
     app.put("/api/workouts", (req, res) => {
-
-    })
+        Workout.findByIdAndUpdate(req.params.id, {$push: {exercises: req.body}})
+        .then(workoutDB => {
+            res.json(workoutDB);
+        })
+        .catch(err => {
+            res.status(400).json(err)
+        })
+    });
 
     //get route
     app. get("api/workouts", (req, res) => {
-        
+        Workout.find({}).then(workoutDB => {
+            res.json(workoutDB);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        })
     })
-
 };
